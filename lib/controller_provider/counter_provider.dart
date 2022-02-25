daacb1aa7818f98b9da13e64b8348bb61b976848
Paulo Santos - AMD <afonso.mea@gmail.com>
2022-02-24 22:07:48 -0300

    Criação de getters para o Provider

--- lib/controller_provider/counter_provider.dart
+++ lib/controller_provider/counter_provider.dart
@@ -1,13 +1,31 @@
-import 'package:provider/provider.dart';
 import 'package:flutter/material.dart';
 
 //Todos os providers terão o identificador
 //with ChangeNotifier, exemplo abaixo
 class Counter with ChangeNotifier {
+
+
   //Declarar provider(Variavel)
   int _count = 0;
 
-  //Criar metodos de get e set para podermos
-  //Pegar esses dados do lado de fora
+  //Criar getters e setters para podermos
+  //pegar esses dados do lado de fora
+  int get count => _count;
+
+  void incrementar() {
+    _count++;
+    notifyListeners();
+  }
+
+  void resetar() {
+    _count =0;
+    notifyListeners();
+  }
+
+  void decrementar() {
+    _count--;
+    notifyListeners();
+  }
+
 
 }
